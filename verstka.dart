efd059af73f30b9aa018ac949455353482ac4706
sergannn <su.vasiliev@gmail.com>
2025-09-16 13:22:02 +0300

    Update verstka.dart
    formatted

--- verstka.dart
+++ verstka.dart
@@ -10,107 +10,115 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
-      home: Scaffold(body: 
-               Column(children:[
-                       //строка
-             verh(),
-                       Center(child:Text("hello")),
-             forImage(),
-                 Center(child:Text("some text")),
-           
-                 Row(
-                    mainAxisAlignment:MainAxisAlignment.spaceEvenly,
-                   children:[
-                             Container(child:
-                 Column(
+      home: Scaffold(
+        body: Column(
+          children: [
+            //строка
+            verh(),
+            Center(child: Text("hello")),
+            forImage(),
+            Center(child: Text("some text")),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  child: Column(
+                    children: [
+                      Row(
                         children: [
-                                Row(
-                                 
-                                children: [
-                                    Container(
-                                    child: Column(
-                                        children: [
-                                        Text(
-                                            'Текст 1',
-                                            style: TextStyle(color: Colors.blue),
-                                        ),
-                                        Text(
-                                            'Текст 2',
-                                            style: TextStyle(color: Colors.red),
-                                        ),
-                                        ],
-                                    ),
-                                    ),
-                                    Container(
-                                    child: Column(
-                                        children: [
-                                        Text(
-                                            'Текст 1',
-                                            style: TextStyle(color: Colors.blue),
-                                        ),
-                                        Text(
-                                            'Текст 2',
-                                            style: TextStyle(color: Colors.red),
-                                        ),
-                                        ],
-                                    ),
-                                    )
-                                ]
+                          Container(
+                            child: Column(
+                              children: [
+                                Text(
+                                  'Текст 1',
+                                  style: TextStyle(color: Colors.blue),
                                 ),
-                                Row(
-                                children: [
-                                    Container(
-                                    child: Column(
-                                        children: [
-                                        Text(
-                                            'Текст 1',
-                                            style: TextStyle(color: Colors.blue),
-                                        ),
-                                        Text(
-                                            'Текст 2',
-                                            style: TextStyle(color: Colors.red),
-                                        ),
-                                        ],
-                                    ),
-                                    ),
-                                    Container(
-                                    child: Column(
-                                        children: [
-                                        Text(
-                                            'Текст 1',
-                                            style: TextStyle(color: Colors.blue),
-                                        ),
-                                        Text(
-                                            'Текст 2',
-                                            style: TextStyle(color: Colors.red),
-                                        ),
-                                        ],
-                                    ),
-                                    )
-                                ]
-                                )
-                                    ]
-)),Text("Helllooooo")])
-               ])));
-                 
-                 
+                                Text(
+                                  'Текст 2',
+                                  style: TextStyle(color: Colors.red),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Container(
+                            child: Column(
+                              children: [
+                                Text(
+                                  'Текст 1',
+                                  style: TextStyle(color: Colors.blue),
+                                ),
+                                Text(
+                                  'Текст 2',
+                                  style: TextStyle(color: Colors.red),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Container(
+                            child: Column(
+                              children: [
+                                Text(
+                                  'Текст 1',
+                                  style: TextStyle(color: Colors.blue),
+                                ),
+                                Text(
+                                  'Текст 2',
+                                  style: TextStyle(color: Colors.red),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Container(
+                            child: Column(
+                              children: [
+                                Text(
+                                  'Текст 1',
+                                  style: TextStyle(color: Colors.blue),
+                                ),
+                                Text(
+                                  'Текст 2',
+                                  style: TextStyle(color: Colors.red),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Text("Helllooooo"),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
-  Widget verh()
-  {
-    return    Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children:[   
-                        Container(width:100,height:100,color:Colors.blue),
-                        Container(width:100,height:100,color:Colors.red)
-                                    ]);//конец строки
+  Widget verh() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Container(width: 100, height: 100, color: Colors.blue),
+        Container(width: 100, height: 100, color: Colors.red),
+      ],
+    ); //конец строки
   }
-  Widget forImage()
-  {
-    return           Center(child:Container(
-                         height:200,                   child:Center(child:Container(color:Colors.yellow,height:50)),
-                         color:Colors.red));
-                       Container(height:20,width:100,color:Colors.green);
+
+  Widget forImage() {
+    return Center(
+      child: Container(
+        height: 200,
+        child: Center(child: Container(color: Colors.yellow, height: 50)),
+        color: Colors.red,
+      ),
+    );
+    Container(height: 20, width: 100, color: Colors.green);
   }
 }
