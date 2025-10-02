a9efdb3199c112d3677897da1de97e5bea9420af
sergannn <su.vasiliev@gmail.com>
2025-10-02 16:27:58 +0300

    Create verstka0210.dart

---
+++ verstka0210.dart
@@ -0,0 +1,303 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
+  //крч этот код с обсервером работает, как я понял по форумам на мобилке, для web и пк - другие методы ( для них надо делать др продключения)
+  //ну и надо подписаться и добавить на уничтожение обсервера для
+  //Проверка mounted перед обновлением состояния
+  //addPostFrameCallback для безопасного доступа к контексту
+  /*
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.resumed && mounted) {
+      // Ждём пока кадр отрисуется, чтобы у контекста всё было готово
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        if (mounted) {
+          _showMessageBox("Добро пожаловать обратно 👋");
+        }
+      });
+    }
+  }
+
+  /// Метод для вывода MessageBox (AlertDialog)
+  void _showMessageBox(String message) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text("Сообщение"),
+        content: Text(message),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: const Text("ОК"),
+          ),
+        ],
+      ),
+    );
+  }
+*/
+  @override
+  Widget build(BuildContext context) {
+
+  //Navigator()
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton(onPressed: 
+        (){
+          
+  Navigator.push(
+  context,
+  MaterialPageRoute<void>(
+    builder: (context) => const MyApp(),
+  ),
+);
+        }),
+        extendBodyBehindAppBar: true,
+
+        appBar: AppBar(
+          shadowColor: Colors.purple,
+          scrolledUnderElevation: 0,
+          foregroundColor: Colors.red,
+          backgroundColor: Colors.green,
+          actionsPadding: EdgeInsetsGeometry.all(10),
+          //  EdgeInsets.all(10),
+          toolbarHeight: 100,
+          actions: [
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage("assets/profile.jpg"),
+            ),
+          ],
+          title: const Text(
+            "Привет, Пользователь!",
+            style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+          ),
+        ),
+        bottomNavigationBar: Row(
+          mainAxisAlignment:
+              MainAxisAlignment.spaceAround, // Равномерное распределение
+          children: const [
+            Icon(Icons.settings),
+            Icon(Icons.exit_to_app),
+            Icon(Icons.person_off_outlined),
+          ],
+        ),
+
+        backgroundColor: const Color(0xFFF5EEDC),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // buildHeader(),
+                  const SizedBox(height: 20),
+                  buildInfoCards(),
+                  const SizedBox(height: 20),
+                  buildPostCard(),
+                  SizedBox(height: 300),
+                  Text("end"),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildHeader() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        const CircleAvatar(
+          radius: 50,
+          backgroundImage: AssetImage("assets/profile.jpg"),
+        ),
+      ],
+    );
+  }
+
+  Widget buildInfoCards() {
+    return Row(
+      children: [
+        Expanded(
+          child: Container(
+            padding: const EdgeInsets.all(16),
+            margin: const EdgeInsets.only(right: 8),
+            decoration: BoxDecoration(
+              // для декорации
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(16),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Icon(Icons.notifications, color: Colors.yellow),
+                SizedBox(height: 8),
+                Text(
+                  "10 новостей",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Expanded(
+          child: Container(
+            padding: const EdgeInsets.all(16),
+            margin: const EdgeInsets.only(left: 8),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(16),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Icon(Icons.event, color: Colors.green),
+                SizedBox(height: 8),
+                Text(
+                  "15 событий",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget buildPostCard() {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              const CircleAvatar(
+                radius: 30,
+                backgroundImage: AssetImage("assets/profile1.jpg"),
+              ),
+              const SizedBox(width: 10),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  Text(
+                    "Сергий Мелник",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text("1 час назад", style: TextStyle(color: Colors.grey)),
+                ],
+              ),
+              const Spacer(),
+              const Icon(Icons.more_vert),
+            ],
+          ),
+          const SizedBox(height: 16),
+
+          const Text("Мой отдых 🌵", style: TextStyle(fontSize: 16)),
+          const SizedBox(height: 16),
+
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                  height: 500,
+                  margin: const EdgeInsets.only(right: 4),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.circular(15),
+                    image: const DecorationImage(
+                      image: AssetImage("assets/photo.jpg"),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                //делим пространство на лапопам
+                child: Column(
+                  children: [
+                    Container(
+                      height: 220,
+                      margin: const EdgeInsets.only(bottom: 4),
+                      decoration: BoxDecoration(
+                        color: Colors.grey[300],
+                        borderRadius: BorderRadius.circular(15),
+                        image: const DecorationImage(
+                          image: AssetImage("assets/photo1.jpg"),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 270,
+                      decoration: BoxDecoration(
+                        color: Colors.grey[300],
+                        borderRadius: BorderRadius.circular(15),
+                        image: const DecorationImage(
+                          image: AssetImage("assets/photo2.jpg"),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 20),
+          Row(
+            mainAxisAlignment:
+                MainAxisAlignment.spaceAround, // Равномерное распределение
+            children: const [
+              Icon(Icons.favorite_border),
+              Icon(Icons.chat_bubble_outline),
+              Icon(Icons.share),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
